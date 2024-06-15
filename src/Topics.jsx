54ad4e2380c7f90c0283c5a1fa10e7218334cc87
just_a_mango <horace.hoff@gmail.com>
2024-06-16 00:43:23 +0200

    /

--- src/Topics.jsx
+++ src/Topics.jsx
@@ -1,5 +1,6 @@
 export const topics = [
     "Nuclear Fusion",
+    "Nuclear Fission",
     "Python",
     "C"
 ]
@@ -12,15 +13,21 @@ export const topics_urls = [
         densities: ["Recommended - 92", 94, 94, 64, 85]
     },
     {
+        topic: "Nuclear Fission",
+        urls: ["https://understand-energy.stanford.edu/energy-resources/nuclear-energy/nuclear-fission","https://www.britannica.com/science/nuclear-fission","https://personal.ems.psu.edu/~radovic/Chapter13.pdf","https://www.pugetsound.edu/sites/default/files/file/7579_Bohr%20liquid%20drop_0.pdf","https://www.epj-conferences.org/articles/epjconf/pdf/2018/24/epjconf_eps-sif2018_00013.pdf"],
+        titles: ["Nuclear Fission | Understand Energy Learning Hub","Nuclear fission | Examples & Process | Britannica","Chapter 13 NUCLEAR FISSION","The Mechanism of Nuclear Fission","Energy from nuclear fission"],
+        densities: [94, 90, 87,69, 89]
+    },
+    {
         topic: "Python",
-        urls: ["https://learnxinyminutes.com/docs/python/", "https://learnpython.org/", "https://thepythoncodingbook.com/book-outline/"],
-        titles: ['Learn Python in Y Minutes', "Learn Python - Free Interactive Python Tutorial", "Learn Python Coding - The Python Coding Book"],
-        densities: ["96", 89, 81]
+        urls: ["https://learnxinyminutes.com/docs/python/", "https://learnpython.org/", "https://thepythoncodingbook.com/book-outline/","https://futurecoder.io/"],
+        titles: ['Learn Python in Y Minutes', "Learn Python - Free Interactive Python Tutorial", "Learn Python Coding - The Python Coding Book","futurecoder: learn python from scratch"],
+        densities: [96, 89, 81, 93]
     },
     {
         topic: "C",
-        urls: ["https://www.tutorialspoint.com/cprogramming/index.htm","https://beej.us/guide/bgc/html/","https://www.cprogramming.com/tutorial/c-tutorial.html"],
-        titles: ["C Tutorial","Beej's Guide to C Programming","C Tutorial - Learn C - Cprogramming.com"],
-        densities: [92,62,85]
+        urls: ["https://www.tutorialspoint.com/cprogramming/index.htm","https://beej.us/guide/bgc/html/","https://www.cprogramming.com/tutorial/c-tutorial.html","https://www.learn-c.org/","https://users.cs.cf.ac.uk/Dave.Marshall/C/"],
+        titles: ["C Tutorial","Beej's Guide to C Programming","C Tutorial - Learn C - Cprogramming.com","Learn C - Free Interactive C Tutorial","Programming in C"],
+        densities: [92,62,85,93,92]
     }
 ]
