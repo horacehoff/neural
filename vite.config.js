24a12af5e72232b705abc04fe9bf8c8b1d428733
just_a_mango <horace.hoff@gmail.com>
2024-06-15 22:47:33 +0200

    /

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import swc from 'unplugin-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), swc.vite()],
+  build: {
+    cssMinify: "lightningcss"
+  },
+  css: {
+    transformer: "lightningcss"
+  }
 })
