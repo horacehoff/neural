eb6a5ec5bc38b6b385cadee345dd108ac9c86f70
just_a_mango <horace.hoff@gmail.com>
2024-06-17 01:04:52 +0200

    /

--- src/Topics.jsx
+++ src/Topics.jsx
@@ -2,7 +2,8 @@ export const topics = [
     "Nuclear Fusion",
     "Nuclear Fission",
     "Python",
-    "C"
+    "C",
+    "Mechanics",
 ]
 
 export const topics_urls = [
@@ -26,8 +27,14 @@ export const topics_urls = [
     },
     {
         topic: "C",
-        urls: ["https://www.tutorialspoint.com/cprogramming/index.htm","https://beej.us/guide/bgc/html/","https://www.cprogramming.com/tutorial/c-tutorial.html","https://www.learn-c.org/","https://users.cs.cf.ac.uk/Dave.Marshall/C/"],
-        titles: ["C Tutorial","Beej's Guide to C Programming","C Tutorial - Learn C - Cprogramming.com","Learn C - Free Interactive C Tutorial","Programming in C"],
-        densities: [92,62,85,93,92]
+        urls: ["https://www.tutorialspoint.com/cprogramming/index.htm","https://beej.us/guide/bgc/html/","https://www.cprogramming.com/tutorial/c-tutorial.html","https://www.learn-c.org/","https://users.cs.cf.ac.uk/Dave.Marshall/C/","https://learnxinyminutes.com/docs/c/"],
+        titles: ["C Tutorial","Beej's Guide to C Programming","C Tutorial - Learn C - Cprogramming.com","Learn C - Free Interactive C Tutorial","Programming in C","Learn C in Y Minutes"],
+        densities: [92,62,85,93,92,70]
+    },
+    {
+        topic: "Mechanics",
+        urls: ["https://www1.grc.nasa.gov/beginners-guide-to-aeronautics/newtons-laws-of-motion/","https://byjus.com/physics/laws-of-motion/","https://justenergy.com/blog/potential-and-kinetic-energy-explained/","https://www.albert.io/blog/equation-for-momentum",'https://www.khanacademy.org/science/physics/work-and-energy/work-and-energy-tutorial/a/what-is-conservation-of-energy','https://openstax.org/books/physics/pages/6-3-rotational-motion','https://physics.uwo.ca/~mhoude2/courses/PDF%20files/physics1501/Ch5-Gravitation.pdf','https://inspiritvr.com/collision-study-guide/',"https://phys.libretexts.org/Bookshelves/University_Physics/Physics_(Boundless)/15%3A_Waves_and_Vibrations/15.3%3A_Periodic_Motion","https://phys.libretexts.org/Courses/Berea_College/Introductory_Physics%3A_Berea_College/07%3A_Work_and_energy/7.01%3A_Work"],
+        titles: ["Newton’s Laws of Motion | Glenn Research Center | NASA","Newton's Laws of Motion - Three Laws of Motion Explanation, Examples","Kinetic Energy and Potential Energy Explained","Momentum: Explanation, Review, and Examples | Albert Resources", 'What is conservation of energy? (article) | Khan Academy','6.3 Rotational Motion - Physics | OpenStax','Chapter 5. Gravitation','Collision Study Guide - Inspirit Learning Inc', '15.3: Periodic Motion - Physics LibreTexts',"7.1: Work - Physics LibreTexts"],
+        densities: [98,51,78,64,73,90,52,77,61,86]
     }
 ]
