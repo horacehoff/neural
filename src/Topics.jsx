b1445c1eaba384b241441e25fd7b607644f947d6
just_a_mango <horace.hoff@gmail.com>
2024-06-18 01:01:44 +0200

    /

--- src/Topics.jsx
+++ src/Topics.jsx
@@ -11,7 +11,7 @@ export const topics_urls = [
         topic: "Nuclear Fusion",
         urls: ['https://fusionforenergy.europa.eu/what-is-fusion/','https://www.iaea.org/bulletin/what-is-fusion-and-why-is-it-so-difficult-to-achieve', 'https://www.energy.gov/science/doe-explainsnuclear-fusion-reactions', 'https://www.space.com/what-is-nuclear-fusion', 'https://www.eia.gov/energyexplained/nuclear/'],
         titles: ['Nuclear Fusion Energy: the energy of the Stars - Fusion for Energy','What is Fusion, and Why Is It So Difficult to Achieve? | IAEA', 'DOE Explains...Nuclear Fusion Reactions | Department of Energy', 'What is nuclear fusion? | Space', 'Nuclear explained - U.S. Energy Information Administration (EIA)'],
-        densities: ["Recommended - 92", 94, 94, 64, 85]
+        densities: [92, 94, 94, 64, 85]
     },
     {
         topic: "Nuclear Fission",
