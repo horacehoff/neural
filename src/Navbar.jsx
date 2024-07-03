d07f9a700d8e2a071f362401cdc221d07c91cc11
just_a_mango <horace.hoff@gmail.com>
2024-07-03 13:18:57 +0200

    /

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
             <a className="neural" href="/">Neural</a>
             {/*<a className="credits" href="/credits">Credits</a>*/}
             <a className="about" href="https://just-a-mango.github.io" target="_blank">About</a>
-            <a className="inspire" id="inspire" href="https://www.spacex.com/careers/jobs/" target="_blank">GitHub</a>
+            <a className="inspire" id="inspire" href="https://github.com/just-a-mango" target="_blank">GitHub</a>
         </>
     )
 }
