60aa7b6516e095962b3ed01830eb7a0067de6079
just_a_mango <horace.hoff@gmail.com>
2024-07-15 01:25:41 +0200

    /

--- src/Home.jsx
+++ src/Home.jsx
@@ -25,13 +25,46 @@ function Home() {
         };
     }
 
+    function absolutePosition(el) {
+        var
+            found,
+            left = 0,
+            top = 0,
+            width = 0,
+            height = 0,
+            offsetBase = absolutePosition.offsetBase;
+        if (!offsetBase && document.body) {
+            offsetBase = absolutePosition.offsetBase = document.createElement('div');
+            offsetBase.style.cssText = 'position:absolute;left:0;top:0';
+            document.body.appendChild(offsetBase);
+        }
+        if (el && el.ownerDocument === document && 'getBoundingClientRect' in el && offsetBase) {
+            var boundingRect = el.getBoundingClientRect();
+            var baseRect = offsetBase.getBoundingClientRect();
+            found = true;
+            left = boundingRect.left - baseRect.left;
+            top = boundingRect.top - baseRect.top;
+            width = boundingRect.right - boundingRect.left;
+            height = boundingRect.bottom - boundingRect.top;
+        }
+        return {
+            found: found,
+            left: left,
+            top: top,
+            width: width,
+            height: height,
+            right: left + width,
+            bottom: top + height
+        };
+    }
+
     useEffect(() => {
         const handleResize = debounce(() => {
             let topics_length = document.querySelectorAll(".searchbox-autocomplete.searchbox-autocomplete-visible ul a").length
             if (topics_length > 0) {
                 document.getElementById(searchbox_autocomplete_id).classList.add("searchbox-autocomplete-visible")
                 let dims = document.getElementById(input_id);
-                let top = dims.getBoundingClientRect().top + document.documentElement.scrollTop
+                let top = absolutePosition(dims).top
                 if (topics_length === 1) {
                     document.getElementById(searchbox_autocomplete_id).style.top = top+50+"px"
                 }
