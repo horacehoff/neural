98fd41dedefd60f63afb4b761debf836957fd5e6
just_a_mango <horace.hoff@gmail.com>
2024-09-09 17:30:02 +0200

    /

--- src/Credits.jsx
+++ src/Credits.jsx
@@ -1,9 +1,11 @@
 import "./Credits.css"
 
 export default function Credits() {
+    let test = "cool"
     return (
         <>
             <h1 className="title credits-title">CREDITS</h1>
+        
         </>
     )
 }
